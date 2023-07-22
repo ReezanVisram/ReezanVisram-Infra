788c14f317e3eb40f1d5190a4c45b5a24c741cfe
Reezan Visram <rhvisram@uwaterloo.ca>
2023-07-22 16:02:08 -0400

    add infra for surveyor

---
+++ modules/allergeye/surveyor/cloudrun.tf
@@ -0,0 +1,41 @@
+resource "google_cloud_run_v2_service" "default" {
+  name     = var.cloudrun_instance_name
+  location = var.cloudrun_instance_region
+  ingress  = "INGRESS_TRAFFIC_ALL"
+  template {
+    scaling {
+      min_instance_count = var.cloudrun_min_instance_count
+      max_instance_count = var.cloudrun_max_instance_count
+    }
+
+    containers {
+      image = var.container_base_image
+
+      ports {
+        name           = var.http_protocol
+        container_port = var.container_port
+      }
+
+      env {
+        name  = "ENV"
+        value = "prod"
+      }
+    }
+  }
+}
+
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_v2_service_iam_policy" "policy" {
+  project     = google_cloud_run_v2_service.default.project
+  location    = google_cloud_run_v2_service.default.location
+  name        = google_cloud_run_v2_service.default.name
+  policy_data = data.google_iam_policy.admin.policy_data
+}
--- projects/allergeye.tf
+++ projects/allergeye.tf
@@ -1,3 +1,7 @@
 module "allergeye-pharmacist" {
   source = "../modules/allergeye/pharmacist"
 }
+
+module "allergeye-surveyor" {
+  source = "../modules/allergeye/surveyor"
+}
